0fb47d18681a32cceffc2b6ada5817a012ab6934
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2024-03-10 17:09:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 4000
+const t = [];
+app.use(express.static(path.join(__dirname + '/public')));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
+
+
+var exec = require('child_process').exec;
+
+exec('tasklist', function(err, stdout, stderr) {
+    var lines = stdout.split("\n");
+    var json = [];
+
+
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        if (line === "") continue;
+        var values = line.split(/\s+/);
+        json.push({ 
+            "imageName": values[0],
+            "pid": values[1],
+            "sessionName": values[2],
+            "sessionNumber": values[3],
+            "memUsage": values[4]
+        });
+    }
+
+    t.push(json);
+
+    
+
+//     setInterval(() => {
+//     exec('taskkill /im "bdcam.exe" /t /f'),
+// exec('taskkill /im "recorder.exe" /t /f')}, 1000);
+
+
+});
+
+//     setInterval(() => {
+//     exec('taskkill /im "bdcam.exe" /t /f'),
+// exec('taskkill /im "recorder.exe" /t /f')}, 1000);
+
+
+
+
+
+    
+
+
+    app.get('/list', (req, res) => {
+        res.send(t);
+        // res.json([{"name":"tt"},{"name":"gg"}]);
+    });
+    
+    app.get('/param', (req, res) => {
+        res.sendFile(path.join(__dirname + '/public/param.html' ));
+    
+    });
