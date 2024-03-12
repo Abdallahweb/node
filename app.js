c175d1cb6da8be2eb454319f101ca08c8d441f7c
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2024-03-12 14:59:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path');
 const app = express()
 const port = 4000
 const t = [];
+app.use(express.static(path.join(__dirname + '/public')));
 
 
 app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname + '/public/index.html' ));
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
@@ -14,28 +15,27 @@ app.listen(port, () => {
 })
 
 
+var exec = require('child_process').exec;
 
-// var exec = require('child_process').exec;
-
-// exec('tasklist', function(err, stdout, stderr) {
-//     var lines = stdout.split("\n");
-//     var json = [];
+exec('tasklist', function(err, stdout, stderr) {
+    var lines = stdout.split("\n");
+    var json = [];
 
 
-//     for (var i = 0; i < lines.length; i++) {
-//         var line = lines[i].trim();
-//         if (line === "") continue;
-//         var values = line.split(/\s+/);
-//         json.push({ 
-//             "imageName": values[0],
-//             "pid": values[1],
-//             "sessionName": values[2],
-//             "sessionNumber": values[3],
-//             "memUsage": values[4]
-//         });
-//     }
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        if (line === "") continue;
+        var values = line.split(/\s+/);
+        json.push({ 
+            "imageName": values[0],
+            "pid": values[1],
+            "sessionName": values[2],
+            "sessionNumber": values[3],
+            "memUsage": values[4]
+        });
+    }
 
-//     t.push(json);
+    t.push(json);
 
     
 
@@ -44,17 +44,7 @@ app.listen(port, () => {
 // exec('taskkill /im "recorder.exe" /t /f')}, 1000);
 
 
-// });
-
-
-// var{exec , spawn} = require('child_process');
-// exec('tasklist.exe' , function(err , stdout,stderr){
-// if(err) console.error(stderr);
-//         t.push(stdout);
-// });
-
-  
-
+});
 //     setInterval(() => {
 //     exec('taskkill /im "bdcam.exe" /t /f'),
 // exec('taskkill /im "recorder.exe" /t /f')}, 1000);
@@ -62,6 +52,8 @@ app.listen(port, () => {
 
 
 
+
+    
 
 
     app.get('/list', (req, res) => {
