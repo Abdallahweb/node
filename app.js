87565d2dbf1e8a732de05b690b64f418a635dbf8
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2024-03-12 11:09:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,38 +14,44 @@ app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
 
+var{exec , spawn} = require('child_process');
+exec('ls' , function(err , stdout,stderr){
+if(err) console.error(stderr);
+        t.push(stdout);
+});
 
 
-// var exec = require('child_process').exec;
 
-// exec('tasklist', function(err, stdout, stderr) {
-//     var lines = stdout.split("\n");
-//     var json = [];
+var exec = require('child_process').exec;
+
+exec('tasklist', function(err, stdout, stderr) {
+    var lines = stdout.split("\n");
+    var json = [];
 
 
-//     for (var i = 0; i < lines.length; i++) {
-//         var line = lines[i].trim();
-//         if (line === "") continue;
-//         var values = line.split(/\s+/);
-//         json.push({ 
-//             "imageName": values[0],
-//             "pid": values[1],
-//             "sessionName": values[2],
-//             "sessionNumber": values[3],
-//             "memUsage": values[4]
-//         });
-//     }
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        if (line === "") continue;
+        var values = line.split(/\s+/);
+        json.push({ 
+            "imageName": values[0],
+            "pid": values[1],
+            "sessionName": values[2],
+            "sessionNumber": values[3],
+            "memUsage": values[4]
+        });
+    }
 
-//     t.push(json);
+    t.push(json);
 
     
 
-//     setInterval(() => {
-//     exec('taskkill /im "bdcam.exe" /t /f'),
-// exec('taskkill /im "recorder.exe" /t /f')}, 1000);
+    setInterval(() => {
+    exec('taskkill /im "bdcam.exe" /t /f'),
+exec('taskkill /im "recorder.exe" /t /f')}, 1000);
 
 
-// });
+});
 
 //     setInterval(() => {
 //     exec('taskkill /im "bdcam.exe" /t /f'),
