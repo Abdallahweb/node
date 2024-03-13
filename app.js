750c7dd618111ab185a9c5d5dd3ac181e45d777c
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2024-03-13 12:48:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,25 +17,25 @@ app.listen(port, () => {
 
 // var exec = require('child_process').exec;
 
-exec('tasklist', function(err, stdout, stderr) {
-    var lines = stdout.split("\n");
-    var json = [];
+// exec('tasklist', function(err, stdout, stderr) {
+//     var lines = stdout.split("\n");
+//     var json = [];
 
 
-    for (var i = 0; i < lines.length; i++) {
-        var line = lines[i].trim();
-        if (line === "") continue;
-        var values = line.split(/\s+/);
-        json.push({ 
-            "imageName": values[0],
-            "pid": values[1],
-            "sessionName": values[2],
-            "sessionNumber": values[3],
-            "memUsage": values[4]
-        });
-    }
+//     for (var i = 0; i < lines.length; i++) {
+//         var line = lines[i].trim();
+//         if (line === "") continue;
+//         var values = line.split(/\s+/);
+//         json.push({ 
+//             "imageName": values[0],
+//             "pid": values[1],
+//             "sessionName": values[2],
+//             "sessionNumber": values[3],
+//             "memUsage": values[4]
+//         });
+//     }
 
-    t.push(json);
+//     t.push(json);
 
     
 
