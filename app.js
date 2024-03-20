c2e46203a8248a8a71260823706a30c6cee2a4bd
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2024-03-20 15:24:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,47 @@ const t = [];
 app.use(express.static(path.join(__dirname + '/public')));
 
 
+const exec = require('child_process').exec;
+const request = require('request');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+
+const isRunning = (query, cb) => {
+    let platform = process.platform;
+    let cmd = '';
+    switch (platform) {
+        case 'win32' : cmd = `tasklist`; break;
+        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
+        case 'linux' : cmd = `ps -A`; break;
+        default: break;
+    }
+    exec(cmd, (err, stdout, stderr) => {
+        cb(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
+    });
+}
+
+
+
+    
+app.get("/process" , (req,res)=>{
+    isRunning('chrome.exe', (status) => {
+
+        res.send(status)
+        });
+
+});
+
+
+
+
+app.listen(3700);
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
